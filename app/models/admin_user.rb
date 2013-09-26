98f793704846d4a18ed95038e032a9757ad5f1d9
chetan kapoor <chetan.kapoor@venturepact.com>
2013-09-26 16:35:50 +0530

    Named Scopes

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,7 @@
 class AdminUser < ActiveRecord::Base
   # attr_accessible :title, :body
   # set_table_name("admin_users")
+  
+  scope :named, lambda{|first,last| where(:first_name => first,:last_name => last)}
+  
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,7 @@
 class Subject < ActiveRecord::Base
   attr_accessible :title, :body, :name, :position, :visible
+  scope :visible, where(:visible => true)
+  scope :invisible, where(:visible => false)
+  scope :search, lambda{|query| where(["name LIKE ?", "%#{query}%"])}
+  
 end
