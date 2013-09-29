d31c28d849d039c31cb1d5b4c0dda8df86969abb
chetan kapoor <chetan.kapoor@venturepact.com>
2013-09-29 12:07:41 +0530

    Adding Associations

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,9 @@
 class Page < ActiveRecord::Base
   # attr_accessible :title, :body
+  attr_accessible :name, :permalink, :position
+  
+  
+  belongs_to :subject
+  has_many :sections
+  
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,22 @@
 class Subject < ActiveRecord::Base
   attr_accessible :title, :body, :name, :position, :visible
+  
+  # has_one :page
+    #has_one methods
+    #subject.page
+    #subject.page = page
+    
+  #has_many methods
+     #subject.pages
+     #subject.pages << page 
+     #subject.pages = [page,page,page]
+     #subject.pages.delete(page)
+     #subject.pages.clear
+     #subject.pages.empty?
+     #subject.pages.size
+     
+  has_many :pages
+  
   scope :visible, where(:visible => true)
   scope :invisible, where(:visible => false)
   scope :search, lambda{|query| where(["name LIKE ?", "%#{query}%"])}
